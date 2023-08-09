46811b8b523639153cdf6c7ad86ee4b7b56c84f1
Prajwal Shenoy <prajwalkpshenoy@gmail.com>
2023-08-09 01:39:27 -0400

    Initial commit

---
+++ src/main/java/com/blogBackend/blogBackendService/BlogBackendServiceApplication.java
@@ -0,0 +1,13 @@
+package com.blogBackend.blogBackendService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BlogBackendServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BlogBackendServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blogBackend/blogBackendService/BlogBackendServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blogBackend.blogBackendService;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BlogBackendServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
